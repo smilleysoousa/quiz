e0782a7b4523a31488f6fe63d7d0c744cc85583a
smilleysousa@gmail.com <60409404+smilleysoousa@users.noreply.github.com>
2021-10-09 00:17:39 -0300

    langs

--- play.py
+++ play.py
@@ -1,10 +1,17 @@
-from commands import COMMAND_EXIT
-from settings import GAME_NAME
+from commands import COMMAND_EXIT, COMMAND_CREATE_PLAYER
+from settings import GAME_NAME, LANG, LANGS
 
 run = True
+
+players = []
 
 while run:
     command = input(f"{GAME_NAME} >> ")
 
     if command == COMMAND_EXIT:
-        run = False 
+        run = False
+
+    if command == COMMAND_CREATE_PLAYER:
+        message = LANGS[LANG]["create_user"]
+        name = input(f"{message}: ")
+        
